e6d17fbf3af19b33a877b2ca76d57e5fac0acd56
Umut Can Aldırmaz <93584517+HopeCoding@users.noreply.github.com>
2023-03-15 20:36:01 +0300

    Add files via upload

---
+++ abstractClasses/src/Main.java
@@ -0,0 +1,20 @@
+package abstractClasses.src;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*GameCalculator gameCalculator=new GameCalculator() {
+            @Override
+            public void hesapla() {
+
+            }
+        }* Abstract larda direk editörümüz üstünü ezmemizi istediği için bu şekilde getiriyor.
+         */
+
+
+    }
+}
